016f2a2816ec18782b666732cf7fa8201876a83b
Tharindu <tharindudissanayake03@gmail.com>
2025-10-09 16:10:54 +0530

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,15 @@ use App\Http\Controllers\Admin\BaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BaseController::class, 'index'])->name('admin.index');
-Route::get('/login', [AuthController::class, 'index'])->name('login.index');
-Route::post('/login', [AuthController::class, 'login'])->name('login.login');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login.index');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::middleware(['can:admin'])->group(function () {
+        // Admin-only routes can be added here
+    });
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
