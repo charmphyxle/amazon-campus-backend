c540e4e2dbf75c6ad19b7fe212d8a60123f0a3bf
Tharindu <tharindudissanayake03@gmail.com>
2025-10-09 16:02:41 +0530

    add auth functions

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -11,4 +11,36 @@ class AuthController extends Controller
     {
         return view('app.admin.login.index');
     }
+
+    public function login(Request $request)
+    {
+         $inputs = $request->validate([
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'min:8', 'max:32'],
+        ]);
+
+        $loginSucceed = auth()->attempt($inputs);
+        if (!$loginSucceed) {
+            //toast('Credentials do not match. Please check user name and password again.', 'error');
+            return redirect()->back();
+        }
+
+        $request->session()->regenerate();
+
+        //toast('User logged in successfully!', 'success');
+        return redirect()->route('admin.index');
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.index')->with([
+            'success' => 'You have logged out successfully.',
+        ]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BaseController::class, 'index'])->name('admin.index');
 Route::get('/login', [AuthController::class, 'index'])->name('login.index');
+Route::post('/login', [AuthController::class, 'login'])->name('login.login');
