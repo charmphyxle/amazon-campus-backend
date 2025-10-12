2a1ea68e4386ad00672512da29d900d3c42374b8
Tharindu <tharindudissanayake03@gmail.com>
2025-10-12 19:34:23 +0530

    clean code

--- resources/views/app/admin/testimonials/index.blade.php
+++ resources/views/app/admin/testimonials/index.blade.php
@@ -73,25 +73,16 @@
                                 </td>
                             </tr>
                         @empty
+                            <tr>
+                                <td colspan="9" align="center">No testimonial found.</td>
+                            </tr>
                         @endforelse
                     </tbody>
                 </table>
             </div>
         </div>
-
         <div class="pagination-area mt-30 mb-50">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-start">
-                    <li class="page-item active"><a class="page-link" href="#">01</a></li>
-                    <li class="page-item"><a class="page-link" href="#">02</a></li>
-                    <li class="page-item"><a class="page-link" href="#">03</a></li>
-                    <li class="page-item"><a class="page-link dot" href="#">...</a></li>
-                    <li class="page-item"><a class="page-link" href="#">16</a></li>
-                    <li class="page-item">
-                        <a class="page-link" href="#"><i class="material-icons md-chevron_right"></i></a>
-                    </li>
-                </ul>
-            </nav>
+            {{ $testimonials->links() }}
         </div>
     </section>
 @endsection
