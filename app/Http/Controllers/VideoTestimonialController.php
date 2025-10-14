0bc7ad666dd7f120cbc1ca0451d6e83a59d31c79
Tharindu <tharindudissanayake03@gmail.com>
2025-10-14 15:01:25 +0530

    create model route functions

---
+++ app/Http/Controllers/VideoTestimonialController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\VideoTestimonial;
+use App\Http\Requests\Admin\StoreVideoTestimonialRequest;
+use App\Http\Requests\Admin\UpdateVideoTestimonialRequest;
+
+class VideoTestimonialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $videoTestimonials = VideoTestimonial::latest()->paginate(10);
+        return view('app.admin.video-testimonials.index', compact('videoTestimonials'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('app.admin.video-testimonials.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVideoTestimonialRequest $request)
+    {
+        $inputs = $request->validated();
+        VideoTestimonial::create($inputs);
+        return back()->with(['success', 'Video testimonial added successfully.']);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VideoTestimonial $videoTestimonial)
+    {
+        return view('app.admin.video-testimonials.edit', compact('videoTestimonial'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVideoTestimonialRequest $request, VideoTestimonial $videoTestimonial)
+    {
+        $inputs = $request->validated();
+        $videoTestimonial->update($inputs);
+        return back()->with(['success', 'Video testimonial updated successfully.']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VideoTestimonial $videoTestimonial)
+    {
+        $videoTestimonial->delete();
+        return back()->with(['success', 'Video testimonial deleted successfully.']);
+    }
+}
---
+++ app/Http/Requests/Admin/UpdateVideoTestimonialRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateVideoTestimonialRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'video' => ['nullable', 'file', 'mimes:mp4,avi,mov,mpeg', 'max:51200'],
+            'title' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
+            'course' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
